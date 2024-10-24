6309ec2a4a20ed51871b367b61928950f6659a63
Your Name <you@example.com>
2024-10-24 12:30:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+user_input = input("Enter number: ")
+number = int(user_input)
+if number  > 7:
+    print("Hello")
+
+
+
+user_name = input("Enter your name: ")
+if user_name == "John":
+    print("Hello, John")
+else:
+    print("There is no such name")
+
+
+
+numeric_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+multiples_of_3 = []
+for number in numeric_array:
+    if number % 3 == 0:
+        multiples_of_3.append(number)
+
+print("Multiples of 3 are: ", multiples_of_3)
